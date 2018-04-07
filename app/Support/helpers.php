58baa674a067e6ea0423475fb2861633fdf45e61
Benjamin <haomingkai@gmail.com>
2018-04-06 23:16:55 -0700

    Added Laravel admin

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -2,14 +2,18 @@
 /**
  * Created by PhpStorm.
  * User: benjamin
- * Date: 2018-04-19
- * Time: 6:14 PM
+ * Date: 2018-04-06
+ * Time: 22:14 PM
  */
 
-namespace App\Support;
-
-
-class helpers
+if (!function_exists('user'))
 {
-
+    function user($driver = null)
+    {
+        if ($driver)
+        {
+            return app('auth')->guard($driver)->user();
+        }
+        return app('auth')->user();
+    }
 }
